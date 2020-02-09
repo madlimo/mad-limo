6b2760ee796eae6cc3e0795263299ddffb123a94
Said Emirmustafaoğlu <madlimo@users.noreply.github.com>
2020-02-09 14:59:35 +0200

    Create temp.py

---
+++ temp.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+def print_twice(x):
+    print(x)
+    print(x)
+def cat_twice(part1, part2):
+    cat = part1 + part2
+    print_twice(cat)
+    
+line1 = 'bing tiddle '
+line2 = 'tiddle bang'
+cat_twice(line1, line2)
